f0f8396fc94a0baede71bfe9277b2bd881a305a0
IsaacJReay <isaacjacksonreay@gmail.com>
2022-06-28 20:23:55 +0700

    Refactor code and add adjustable token exp

--- OnlineLibrary/Controllers/Helper.cs
+++ OnlineLibrary/Controllers/Helper.cs
@@ -23,7 +23,14 @@ public partial class apiController : Controller
     {
         using (HMACSHA512 hmac = new HMACSHA512())
         {
-            return (hmac.Key, hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)));
+            return (
+                hmac.Key,
+                hmac.ComputeHash(
+                    System.Text.Encoding.UTF8.GetBytes(
+                        password
+                    )
+                )
+            );
         }
 
     }
@@ -32,7 +39,11 @@ public partial class apiController : Controller
     {
         using (HMACSHA512 hmac = new HMACSHA512(UserpasswordSalt))
         {
-            byte[] computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(UserPassword));
+            byte[] computeHash = hmac.ComputeHash(
+                System.Text.Encoding.UTF8.GetBytes(
+                    UserPassword
+                )
+            );
             return computeHash.SequenceEqual(UserpasswordHash);
         }
     }
@@ -43,11 +54,22 @@ public partial class apiController : Controller
         {
             new Claim(ClaimTypes.Name, User.UserID, User.UserName)
         };
-        SymmetricSecurityKey key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration.GetSection("JwtSettings:SecretKey").Value));
+        SymmetricSecurityKey key = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(
+                configuration.GetSection(
+                    "JwtSettings:SecretKey"
+                ).Value
+            )
+        );
+        int TokenExpiration = int.Parse(
+            configuration.GetSection(
+                "JwtSettings:TokenExpirationSec"
+            ).Value
+        );
         SigningCredentials cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
         JwtSecurityToken token = await Task.Run(() => new JwtSecurityToken(
             claims: claims,
-            expires: DateTime.Now.AddDays(1),
+            expires: DateTime.Now.AddSeconds(TokenExpiration),
             signingCredentials: cred
         ));
         return await Task.Run(() => new JwtSecurityTokenHandler().WriteToken(token));
@@ -55,27 +77,17 @@ public partial class apiController : Controller
 
     protected (string, string) Upload(IFormFile currentFile, bool isPhoto)
     {
-        string destination = default!;
-        string extension = default!;
-        if (isPhoto)
-        {
-            destination = Path.Combine("/home/isaac/project/OnlineLibrary/", "image");
-        }
-        else
-        {
-            destination = Path.Combine("/home/isaac/project/OnlineLibrary/", "file");
-        }
+        string destination = isPhoto ? Path.Combine("/home/isaac/project/OnlineLibrary/", "images") : Path.Combine("/home/isaac/project/OnlineLibrary/", "files"); ;
 
-        string filename = default!;
         if (!Directory.Exists(destination))
         {
             Directory.CreateDirectory(destination);
         }
 
-        extension = Path.GetExtension(currentFile.FileName);
-        filename = DateTimeOffset.Now.ToUnixTimeSeconds().ToString() + extension;
-
+        string extension = Path.GetExtension(currentFile.FileName);
+        string filename = DateTimeOffset.Now.ToUnixTimeSeconds().ToString() + extension;
         string destinationFile = Path.Combine(destination, filename);
+
         using FileStream fstream = new FileStream(destinationFile, FileMode.Create);
         currentFile.CopyToAsync(fstream);
 
