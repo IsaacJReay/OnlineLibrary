f7fe70c65ce4cabe6af27004486cc492172f4988
IsaacJReay <isaacjacksonreay@gmail.com>
2022-06-28 19:33:22 +0700

    remove filetype for Dto because backend will determine filetype by its extension

--- OnlineLibrary/Models/BookDto.cs
+++ OnlineLibrary/Models/BookDto.cs
@@ -2,7 +2,6 @@ namespace OnlineLibrary.Models;
 
 public class BookDto {
     public string BookID { get; set; } = default!;
-    public Enums.FileType BookFileType { get; set; } = default!;
     public string BookTitle { get; set; } = default!;
     public string TeacherID { get; set; } = default!;
     public DateTime BookDate { get; set; } = default!;
